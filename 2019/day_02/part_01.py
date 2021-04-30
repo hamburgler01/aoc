ff2a0955e2aa9f15faf4ef36f26ddbe8913814fb
Patrick Ryan <patrick.ryan@dominolaser.com>
2021-04-30 12:01:01 +0200

    AOC_2019: day_02 part_01

---
+++ 2019/day_02/part_01.py
@@ -0,0 +1,23 @@
+# Store intcodes as a list of integers.
+with open('./input_01.txt') as input_file:
+    intcodes = [int(num) for num in input_file.read().strip().split(',')]
+
+# Restore 1202 Program alarm state.
+intcodes[1] = 12
+intcodes[2] = 2
+
+# Loop over intcodes and process them.
+index = 0
+while index < len(intcodes):
+    if intcodes[index] == 1:
+        intcodes[intcodes[index + 3]] = intcodes[intcodes[index + 1]] + intcodes[intcodes[index + 2]]
+        index += 4
+    elif intcodes[index] == 2:
+        intcodes[intcodes[index + 3]] = intcodes[intcodes[index + 1]] * intcodes[intcodes[index + 2]]
+        index += 4
+    elif intcodes[index] == 99:
+        break;
+    else:
+        raise Exception(f'Illegal instruction: {intcodes[index]}')
+                                                                                 
+print("intcodes[0] = ", intcodes[0])
